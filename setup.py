921ea7434f284cd86a801e5404dec09b9a22774c
Travis Clarke <travis.m.clarke@gmail.com>
2018-09-07 21:38:24 -0700

    v2.0.0 - shorten BitFlag import statement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bitflag",
-    version="1.0.1",
+    version="2.0.0",
     author="Travis Clarke",
     author_email="travis.m.clarke@gmail.com",
     description="A bit flag class for python",
